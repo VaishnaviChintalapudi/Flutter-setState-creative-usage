321c9e3675c98cc12885fc653b49ab33bf1f81de
= <=>
2025-08-19 07:58:02 +0530

    Added screenshots of the project also attachmed the video recording of my app that changes the shape,emoji,and size of text for every button click using setState()

--- lib/main.dart
+++ lib/main.dart
@@ -38,6 +38,7 @@ class _CounterExampleState extends State<CounterExample> {
 
   Color getRandomColor() {
     final Random random = Random();
+
     return Color.fromARGB(
         255, random.nextInt(256), random.nextInt(256), random.nextInt(256));
   }
